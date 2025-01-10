fa42bf9cc5e5c4deab5dff843527ac8ebbffee6f
Rammamus <adrian.folin@elev.ga.lbs.se>
2025-01-10 09:33:36 +0100

    something

---
+++ Assets/Scripts/EnemyScripts/EnemyMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    NavMeshAgent agent;
+    GameObject player;
+    bool canSeePlayer;
+    float sightDistance = 5;
+
+    private void Start()
+    {
+        player = GameObject.FindObjectOfType<PlayerMovement>().gameObject;
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    private void Update()
+    {
+        if (canSeePlayer)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, player.transform.position - transform.position, sightDistance);
+        if (ray.collider != null)
+        {
+            canSeePlayer = ray.collider.CompareTag("Player");
+            if (canSeePlayer)
+            {
+                Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.green);
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.red);
+            }
+        }
+    }
+}
