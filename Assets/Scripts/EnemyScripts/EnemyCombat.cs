eb2f33658ff14bdf174d79490ae143645d86d1c2
Rammamus <adrian.folin@elev.ga.lbs.se>
2025-01-10 10:23:00 +0100

    yessir

--- Assets/Scripts/EnemyScripts/EnemyCombat.cs
+++ Assets/Scripts/EnemyScripts/EnemyCombat.cs
@@ -4,24 +4,56 @@ using UnityEngine;
 
 public class EnemyCombat : MonoBehaviour
 {
-    public float damage;
-    public float attackSpeed;
+    [Header("Attack Related")]
+    public float attackDMG, attackSpeed, attackSpeedTimer, attackRange;
+    public bool isInRange;
+
+    [Header("HP Related")]
     public float hp, maxHP;
+
+    PlayerCombat player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        hp = maxHP;
+        player = FindObjectOfType<PlayerCombat>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isInRange && attackSpeedTimer >= attackSpeed)
+        {
+            AttackPlayer(attackDMG);
+            attackSpeedTimer = 0;
+        }
+
+        if (attackSpeedTimer < attackSpeed)
+        {
+            attackSpeedTimer += Time.deltaTime;
+        }
     }
 
-    public void AttackPlayer()
+    private void FixedUpdate()
     {
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, player.transform.position - transform.position, attackRange);
+        if (ray.collider != null)
+        {
+            isInRange = ray.collider.CompareTag("Player");
+            if (isInRange)
+            {
+                Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.green);
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.red);
+            }
+        }
+    }
 
+    public void AttackPlayer(float dmg)
+    {
+        player.hp -= dmg;
     }
 }
--- Assets/Scripts/PlayerScripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerScripts/PlayerCombat.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    public float damage;
-    public float attackSpeed;
+    [Header("Attack Related")]
+    public float attackDamage, attackSpeed;
+
+    [Header("HP Related")]
     public float hp, maxHP;
 
     // Start is called before the first frame update
