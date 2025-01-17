c87549ee83cc00c5587d831a179e1c4fbfe67e2b
BoBeliPop <120007768+BoBeliPop@users.noreply.github.com>
2025-01-17 12:29:02 +0100

    asd

--- Assets/Scripts/UI/PanelUpgrademanager.cs
+++ Assets/Scripts/UI/PanelUpgrademanager.cs
@@ -4,54 +4,24 @@ using UnityEngine;
 using System.Linq;
 using UnityEngine.UI;
 
+
 public class PanelUpgrademanager : MonoBehaviour
 {
-    public List<GameObject> item; // List of button prefabs or items
-    public Transform panel; // Parent panel for buttons
+    public List<GameObject> item;
+    public Transform panel;
 
     // Update is called once per frame
     public void OnEnable()
     {
-        // Activate the panel
-        panel.gameObject.SetActive(true);
-
-        // Deactivate all buttons in the panel
         foreach (Transform child in panel)
         {
-            child.gameObject.SetActive(false);
+            Destroy(child.gameObject);
         }
-
-        // Select 3 random items from the list
         System.Random random = new System.Random();
         var selectedItems = item.OrderBy(x => random.Next()).Take(3).ToList();
-
-        // Activate the buttons and set their text
-        for (int i = 0; i < selectedItems.Count; i++)
+        foreach (var item in selectedItems)
         {
-            GameObject button = selectedItems[i];
-            button.SetActive(true); // Activate the button
-
-            // Assuming the button has a Text component as a child
-            Text buttonText = button.GetComponentInChildren<Text>();
-            if (buttonText != null)
-            {
-                buttonText.text = "Upgrade " + (i + 1); // Set button text
-            }
-
-            // Optionally, set up button click events here
-            Button buttonComponent = button.GetComponent<Button>();
-            if (buttonComponent != null)
-            {
-                int index = i; // Capture the index for the event
-                buttonComponent.onClick.AddListener(() => OnUpgradeButtonClicked(index));
-            }
+            GameObject button = Instantiate(item, panel);
         }
-    }
-
-    private void OnUpgradeButtonClicked(int index)
-    {
-        // Handle the button click event
-        Debug.Log("Button " + index + " clicked!");
-        // Add your logic for what happens when the button is clicked
     }
 }
